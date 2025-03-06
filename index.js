38b2c0ad8c05b686f541ebcfcf7f2cfd2327f00d
AgilaThiruchelvam18 <agilaagi18@gmail.com>
2025-03-07 01:00:33 +0530

    commit

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cors({
 }));
 
 app.use(cookieParser());
-
+app.options("*", cors());
 app.use(express.json()); // Parses JSON request body
 app.use(express.urlencoded({ extended: true })); // Parses URL-encoded request body
 
