a4bdd0ec6c190429785961c420826b644428af68
AgilaThiruchelvam18 <agilaagi18@gmail.com>
2025-03-10 16:20:11 +0530

    commit

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -2,22 +2,6 @@ const jwt = require("jsonwebtoken");
 const Trainer = require("../models/Trainer");
 const User = require("../models/User");
 
-exports.protectTrainer = async (req, res, next) => {
-  try {
-    const token = req.cookies.trainer_jwt; // Get token from HTTP-only cookie
-    if (!token) return res.status(401).json({ message: "Unauthorized access" });
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await Trainer.findById(decoded.userId).select("-password");
-
-    if (!req.user) return res.status(401).json({ message: "User not found" });
-console.log("req.user",req.user);
-    next();
-  } catch (error) {
-    res.status(401).json({ message: "Invalid token" });
-  }
-};
-
 exports.protectCustomer = async (req, res, next) => {
     try {
       const token = req.cookies.customer_jwt; // Get token from HTTP-only cookie
@@ -34,3 +18,18 @@ exports.protectCustomer = async (req, res, next) => {
     }
   };
   
+exports.protectTrainer = async (req, res, next) => {
+  try {
+    const token = req.cookies.trainer_jwt; // Get token from HTTP-only cookie
+    if (!token) return res.status(401).json({ message: "Unauthorized access" });
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await Trainer.findById(decoded.userId).select("-password");
+
+    if (!req.user) return res.status(401).json({ message: "User not found" });
+console.log("req.user",req.user);
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
