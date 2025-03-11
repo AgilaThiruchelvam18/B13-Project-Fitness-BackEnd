dcee7ba6c12b2164d86ff9b9dba8dd4f9910d7e3
AgilaThiruchelvam18 <agilaagi18@gmail.com>
2025-03-11 14:22:37 +0530

    c

--- src/routes/bookingRoute.js
+++ src/routes/bookingRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bookingController = require("../controllers/bookingController");
 const { protectCustomer, protectTrainer } = require("../middleware/authMiddleware");
 
-// router.post("/",protectCustomer, bookingController.createBooking);
+router.post("/",protectCustomer, bookingController.createBooking);
 // router.get("/", protectCustomer, bookingController.getMyBookings);
 // router.get("/:id",protectCustomer,  bookingController.getBookingById);
 // router.put("/:id",protectCustomer,  bookingController.updateBooking);
