53cde4cf3b3b7e9623fd560a9e7198c7f1a8f660
AgilaThiruchelvam18 <agilaagi18@gmail.com>
2025-03-12 15:12:58 +0530

    c

--- src/controllers/classController.js
+++ src/controllers/classController.js
@@ -78,7 +78,7 @@ const getTrainerClasses = async (req, res) => {
 // ✅ Get all upcoming classes (for customers)
 const AllTrainerClasses = async (req, res) => {
   try {
-    const classes = await Class.find();
+    const classes = await Class.find().populate("trainer", "userName email ratings");
     res.status(200).json(classes);
   } catch (error) {
     console.error("Error fetching all classes:", error);
