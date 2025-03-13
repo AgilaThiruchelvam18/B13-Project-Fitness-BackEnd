a6f9c534896e475237eb716ff4bbde685bf9b8b0
AgilaThiruchelvam18 <agilaagi18@gmail.com>
2025-03-13 14:03:17 +0530

    c

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { getAllUsers, getUserById, updateUser, deleteUser } = require("../controllers/userController");
+const { protectCustomer, protectTrainer } = require("../middleware/authMiddleware");
 
 router.get("/", getAllUsers);
 router.get("/:id", getUserById);
-router.put("/:id", updateUser);
+router.put("/:id",protectCustomer, updateUser);
 router.delete("/:id", deleteUser);
 
 module.exports = router;
