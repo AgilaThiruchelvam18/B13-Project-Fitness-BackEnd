7943f24e71367db92bcd0dc1fd51c66adcbb32dc
AgilaThiruchelvam18 <agilaagi18@gmail.com>
2025-03-14 01:04:16 +0530

    c

--- src/models/Recommendation.js
+++ src/models/Recommendation.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const recommendationSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  trainer: { type: mongoose.Schema.Types.ObjectId, ref: "Trainer", required: true },
+  trainer: { type: mongoose.Schema.Types.ObjectId, ref: "Class", required: true },
   recommendedClasses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Class" }],
 }, { timestamps: true });
 
