819373ddc887461a238f48fe6e5b712d476cd781
AgilaThiruchelvam18 <agilaagi18@gmail.com>
2025-03-14 13:32:49 +0530

    c

--- src/controllers/authTrainerController.js
+++ src/controllers/authTrainerController.js
@@ -134,7 +134,7 @@ exports.requestPasswordReset = async (req, res) => {
     trainer.resetTokenExpiry = Date.now() + 15 * 60 * 1000; // 15 minutes expiry
     await trainer.save();
 
-    const resetLink = `${process.env.CLIENT_URL}/trainer/reset-password/${resetToken}`;
+    const resetLink = `${process.env.FRONTEND_URL}/trainer/reset-password/${resetToken}`;
     await transporter.sendMail({
       to: trainer.email,
       subject: "Password Reset Request",
