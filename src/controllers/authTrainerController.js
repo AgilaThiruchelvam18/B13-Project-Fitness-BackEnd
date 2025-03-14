1a431ccc8f24e31c300e3ae325a0ca4732add236
AgilaThiruchelvam18 <agilaagi18@gmail.com>
2025-03-14 22:13:59 +0530

    c

--- src/controllers/authTrainerController.js
+++ src/controllers/authTrainerController.js
@@ -192,8 +192,9 @@ exports.resetPassword = async (req, res) => {
 
 exports.getTrainerProfile = async (req, res) => {
   try {
-    res.json(req.user);
-  } catch (error) {
+    const trainer = await Trainer.findById(req.user._id).populate("classes"); // Populate classes
+    res.json(trainer);
+    } catch (error) {
     res.status(500).json({ message: "Server Error", error: error.message });
   }
 };
