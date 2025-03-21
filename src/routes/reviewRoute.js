74f1f89972e6dc1039803ac41bd050a3f9a0a3f0
AgilaThiruchelvam18 <agilaagi18@gmail.com>
2025-03-22 03:03:20 +0530

    c

--- src/routes/reviewRoute.js
+++ src/routes/reviewRoute.js
@@ -5,7 +5,7 @@ const { protectCustomer, protectTrainer } = require("../middleware/authMiddlewar
 const router = express.Router();
 
 router.post("/", protectCustomer, createReview); // Submit Review
-// router.get("/", protectCustomer, getReviews);
+router.get("/", protectCustomer, getReviews);
 router.get("/:trainerId",protectTrainer, getTrainerReviews); // Fetch Reviews for a Trainer
 router.put("/:reviewId/response", protectTrainer, respondToReview); // Trainer responds
 
