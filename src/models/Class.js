aa10822e588d6392a48af64257e620892a8f8b2c
AgilaThiruchelvam18 <agilaagi18@gmail.com>
2025-03-26 01:17:51 +0530

    c

--- src/models/Class.js
+++ src/models/Class.js
@@ -25,8 +25,7 @@
                   date: { type: Date, required: true },
                   day: {
                     type: String,
-                    enum: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-                    required: true,
+                    enum: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
                   },
                   startTime: { type: String, required: true },
                   endTime: { type: String, required: true },
