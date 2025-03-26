0598016c58276e8cf2e08be6eef6ab02546f7d2e
AgilaThiruchelvam18 <agilaagi18@gmail.com>
2025-03-26 11:46:01 +0530

    c

--- src/routes/authTrainerRoute.js
+++ src/routes/authTrainerRoute.js
@@ -11,6 +11,9 @@ const {
 } = require("../controllers/authTrainerController");
 const { protectTrainer } = require("../middleware/authMiddleware");
 const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
 const uploadDir = path.join(__dirname, "uploads");
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
