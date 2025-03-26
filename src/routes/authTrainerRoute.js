ccdafefb354cbb53ac27ea290f2890fc83ab6318
AgilaThiruchelvam18 <agilaagi18@gmail.com>
2025-03-26 11:43:24 +0530

    c

--- src/routes/authTrainerRoute.js
+++ src/routes/authTrainerRoute.js
@@ -11,7 +11,10 @@ const {
 } = require("../controllers/authTrainerController");
 const { protectTrainer } = require("../middleware/authMiddleware");
 const multer = require("multer");
-
+const uploadDir = path.join(__dirname, "uploads");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "uploads/"); // Store files in the "uploads/" directory
