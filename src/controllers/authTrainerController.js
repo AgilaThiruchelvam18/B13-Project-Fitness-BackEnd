d3fdaf0a09f758e9c5cccfebad1044c40edef53c
AgilaThiruchelvam18 <agilaagi18@gmail.com>
2025-03-26 16:01:40 +0530

    c

--- src/controllers/authTrainerController.js
+++ src/controllers/authTrainerController.js
@@ -43,7 +43,7 @@ exports.register = async (req, res) => {
       console.log("Files received:", req.files); // Debugging log
 
       mediaUploads = req.files.map((file) => ({
-        url: `/uploads/${file.filename}`, // Relative path
+        url: `https://fitnesshub-aa.netlify.app/uploads/${file.filename}`, // Relative path
         type: file.mimetype.startsWith("image") ? "image" : "video"
       }));
     } else {
